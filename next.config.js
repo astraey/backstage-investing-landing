d1658931380e43dbfca2dae28be51535454cdb3c
Juan Delmuro <jgmuro@amazon.com>
2023-05-12 22:10:06 -0400

    next.config.js adjusted for Amplify Builds

--- next.config.js
+++ next.config.js
@@ -11,5 +11,8 @@ module.exports = withBundleAnalyzer({
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
-  output: 'standalone',
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
 });
